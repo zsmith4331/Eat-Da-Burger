b251c891357693815574bd037f93052634b39525
Zach Smith <zsmith4331@gmail.com>
2021-03-06 18:01:43 -0500

    setup model

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,22 @@
 // Import the ORM //
 const orm = require('../config/orm.js');
 
+const burger = {
 
-
-
-
-
-
-
+  selctAll(callback) 
+  {
+    orm.selectAll("burgers", (response) => callback(response));
+  },
+  insertOne(burgerName, callback) 
+  {
+    orm.create("burgers", burgerName, (response) => callback(response));
+  },
+  updateOne(devoured, values, callback) 
+  {
+    orm.update("burgers", devoured, "id", values, (response) => callback(response));
+  }
+  
+};
 
 // Export the database functions for the controller (burgers_controller.js) //
 module.exports = burger;
