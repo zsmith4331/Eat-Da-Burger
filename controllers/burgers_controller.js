d0b2cca0501835551533dcc03da250264c2c0e10
Zach Smith <zsmith4331@gmail.com>
2021-03-06 17:37:49 -0500

    setup controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+// Dependencies //
+const express = require("express");
+const router = express.Router();
+
+// Import the model (burger.js) for database functions //
+const burger = require("../models/burger.js");
+
+// Routes //
+router.get("/", (request, response) => {
+    burger.selectAll((results) => {
+      const hbsObject = {
+        burgers: results,
+      };
+      console.log(hbsObject);
+      response.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burgers", (request, response) => {
+    burger.insertOne({ burger_name: request.body.burger_name }, results => {
+        if (results.affectedRows === 0) {
+            // If no rows changed then 404 //
+            return response.status(404).end();
+          }
+          response.status(200).end();
+    });
+  });
+
+  router.put("/api/burgers/:id", (request, response) => {
+      burger.updateOne(request.body, request.params.id, results => {
+        if (results.affectedRows === 0) {
+            // If no rows changed then 404 //
+            return response.status(404).end();
+          }
+          response.status(200).end();
+      });
+  });
+
+  // Export routes for server.js //
+  module.exports = router;
